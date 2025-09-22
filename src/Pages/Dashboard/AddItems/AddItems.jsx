1780713e0596a031243d02962407b71523a2f0c8
jobayerimtiaz <jiefq.37@gmail.com>
2025-09-22 12:55:08 +0600

    Image hosted

--- src/Pages/Dashboard/AddItems/AddItems.jsx
+++ src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -1,14 +1,26 @@
 import { useForm } from "react-hook-form";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import { FaUtensils } from "react-icons/fa";
+import UseAxiosPublic from "../../../Hooks/UseAxiosPublic";
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 const AddItems = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => {
+  const axiosPublic = UseAxiosPublic();
+  const onSubmit = async (data) => {
     console.log(data);
+    //image upload to
+    const imageFile = { image: data.image[0] };
+    const res = await axiosPublic.post(image_hosting_api, imageFile, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    console.log(res.data);
   };
   return (
     <div>
@@ -101,9 +113,10 @@ const AddItems = () => {
           <div>
             <input
               type="file"
-              {...register("file", { required: "File is required" })}
-              className="w-full bg-white text-gray-600"
+              {...register("image", { required: "File is required" })}
+              className="w-full bg-gray-600 text-white rounded-sm p-1"
             />
+
             {errors.file && (
               <p className="text-red-500 text-sm">{errors.file.message}</p>
             )}
